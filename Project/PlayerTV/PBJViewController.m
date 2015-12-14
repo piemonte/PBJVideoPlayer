2aefe714953ab408ec5085205bb1fdb501b43ee7
Leslie Nadeau <leslienadeau@gmail.com>
2015-12-13 17:58:06 -0800

    add tvOS support to the sample project

---
+++ Project/PlayerTV/PBJViewController.m
@@ -0,0 +1,128 @@
+//
+//  PBJViewController.m
+//  Player
+//
+//  Created by Patrick Piemonte on 11/12/13.
+//  Copyright (c) 2013-present, Patrick Piemonte, http://patrickpiemonte.com
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is furnished to do so,
+//  subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import "PBJViewController.h"
+#import "PBJVideoPlayerController.h"
+
+static NSString * const PBJRemoteVideoURLString = @"http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8";
+static NSString * const PBJViewControllerVideoPath = @"https://scontent.cdninstagram.com/hphotos-xfa1/t50.2886-16/11719145_918467924880620_816495633_n.mp4";
+
+@interface PBJViewController () <
+    PBJVideoPlayerControllerDelegate>
+{
+    PBJVideoPlayerController *_videoPlayerController;
+    UIImageView *_playButton;
+}
+
+@end
+
+@implementation PBJViewController
+
+#pragma mark - UIViewController status bar
+
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
+}
+
+#pragma mark - view lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+
+    _videoPlayerController = [[PBJVideoPlayerController alloc] init];
+    _videoPlayerController.delegate = self;
+    _videoPlayerController.view.frame = self.view.bounds;
+    
+    [self addChildViewController:_videoPlayerController];
+    [self.view addSubview:_videoPlayerController.view];
+    [_videoPlayerController didMoveToParentViewController:self];
+
+    _playButton = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"play_button"]];
+    _playButton.center = self.view.center;
+    [self.view addSubview:_playButton];
+    [self.view bringSubviewToFront:_playButton];
+    
+    //_videoPlayerController.videoPath = PBJViewControllerVideoPath;
+    NSURL *bipbopUrl = [[NSURL alloc] initWithString:PBJRemoteVideoURLString];
+    _videoPlayerController.asset = [[AVURLAsset alloc] initWithURL:bipbopUrl options:nil];
+}
+
+#pragma mark - PBJVideoPlayerControllerDelegate
+
+- (void)videoPlayerReady:(PBJVideoPlayerController *)videoPlayer
+{
+    //NSLog(@"Max duration of the video: %f", videoPlayer.maxDuration);
+}
+
+- (void)videoPlayerPlaybackStateDidChange:(PBJVideoPlayerController *)videoPlayer
+{
+}
+
+- (void)videoPlayerBufferringStateDidChange:(PBJVideoPlayerController *)videoPlayer
+{
+    /*switch (videoPlayer.bufferingState) {
+     case PBJVideoPlayerBufferingStateUnknown:
+     NSLog(@"Buffering state unknown!");
+     break;
+     
+     case PBJVideoPlayerBufferingStateReady:
+     NSLog(@"Buffering state Ready! Video will start/ready playing now.");
+     break;
+     
+     case PBJVideoPlayerBufferingStateDelayed:
+     NSLog(@"Buffering state Delayed! Video will pause/stop playing now.");
+     break;
+     default:
+     break;
+     }*/
+}
+
+- (void)videoPlayerPlaybackWillStartFromBeginning:(PBJVideoPlayerController *)videoPlayer
+{
+    _playButton.alpha = 1.0f;
+    _playButton.hidden = NO;
+
+    [UIView animateWithDuration:0.1f animations:^{
+        _playButton.alpha = 0.0f;
+    } completion:^(BOOL finished) {
+        _playButton.hidden = YES;
+    }];
+}
+
+- (void)videoPlayerPlaybackDidEnd:(PBJVideoPlayerController *)videoPlayer
+{
+    _playButton.hidden = NO;
+
+    [UIView animateWithDuration:0.1f animations:^{
+        _playButton.alpha = 1.0f;
+    } completion:^(BOOL finished) {
+    }];
+}
+
+@end
---
+++ Project/PlayerTV/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PlayerTV
+//
+//  Created by Leslie Nadeau on 12/13/15.
+//  Copyright © 2015 Patrick Piemonte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PBJAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PBJAppDelegate class]));
+    }
+}
